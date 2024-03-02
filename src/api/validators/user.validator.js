ae0383b6cb9543409dc628ba2965c0cc8eb33207
krystianklimczak <krystianklimczak1996r.com>
2024-03-02 11:32:13 +0100

    Commented required fileds in Joi validation.

--- src/api/validators/user.validator.js
+++ src/api/validators/user.validator.js
@@ -5,10 +5,10 @@ export function validateRegister(body) {
     email: Joi.string().email().min(3).required(),
     password: Joi.string().min(6).max(20).required(),
     name: Joi.string().min(3).max(24).required(),
-    language: Joi.string().valid('tr', 'en').required(),
-    platform: Joi.string().valid('Android', 'IOS').required(),
-    timezone: Joi.number().required(),
-    deviceId: Joi.string().min(4).required(),
+    language: Joi.string().valid('tr', 'en'),
+    // platform: Joi.string().valid('Android', 'IOS').required(),
+    // timezone: Joi.number().required(),
+    // deviceId: Joi.string().min(4).required(),
   });
   return schema.validate(body);
 }
