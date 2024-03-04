7891aa57d3cfb3f06d2190749c905c02cc39b3da
mattmalicki <mateusz.malicki96@gmail.com>
2024-03-05 00:48:37 +0100

    Creating route for recipes.

---
+++ src/api/routes/recipe.js
@@ -0,0 +1,25 @@
+import { Router } from 'express';
+import {
+  addToFavorites,
+  deleteFromFavorites,
+  getOneRecipe,
+  getPopular,
+  getRecipes,
+} from '../controllers/recipe/index.js';
+// import { auth, imageUpload } from '../middlewares/index.js';
+
+const router = Router();
+
+// CATEGORY
+router.get('/', getRecipes);
+// router.get('/category', auth, getRecipes);
+router.get('/popular', getPopular);
+// router.get('/popular', auth, getPopular);
+router.post('/favorites', addToFavorites);
+// router.post('/favorites', auth, addToFavorites);
+router.delete('/favorites/:recipeId', deleteFromFavorites);
+// router.delete('/favorites/:recipeId', auth, deleteFromFavorites);
+router.get('/:recipeId', getOneRecipe);
+// router.get('/:recipeId', auth, getOneRecipe);
+
+export default router;
