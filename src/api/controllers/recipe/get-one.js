526636081514087f3a5c6dd928504cabf1a80b4e
mattmalicki <mateusz.malicki96@gmail.com>
2024-03-06 08:16:56 +0100

    Changed return key to match other returns and to simplify frontend.

--- src/api/controllers/recipe/get-one.js
+++ src/api/controllers/recipe/get-one.js
@@ -4,11 +4,11 @@ import { getRecipeByIdFromDb } from './helpers.js';
 const getRecipeById = async (req, res, next) => {
   try {
     const { id } = req.params;
-    const recipe = await getRecipeByIdFromDb(id);
+    const recipes = await getRecipeByIdFromDb(id);
     return res.status(200).json({
       resultMassage: { en: getText('en', '00094') },
       resultCode: '00094',
-      recipe,
+      recipes,
     });
   } catch (error) {
     return next(error);
