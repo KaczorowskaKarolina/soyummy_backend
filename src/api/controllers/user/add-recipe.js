76b9a4e1b3dcb0e86fe2bd526cb0983f03a1c97e
mattmalicki <mateusz.malicki96@gmail.com>
2024-03-08 16:01:03 +0100

    Add recipe works!

--- src/api/controllers/user/add-recipe.js
+++ src/api/controllers/user/add-recipe.js
@@ -2,9 +2,9 @@ import { getText } from '../../../utils/index.js';
 import { getUserById } from './helpers.js';
 import { createRecipeToDb } from './helpers.js';
 import { imageApiKey } from '../../../config/index.js';
-// import Jimp from 'jimp';
-// import { nanoid } from 'nanoid';
 import fs from 'fs/promises';
+import { tmpDir } from '../../middlewares/index.js';
+import imgbbUploader from 'imgbb-uploader';
 
 async function addRecipe(req, res, next) {
   try {
@@ -16,6 +16,7 @@ async function addRecipe(req, res, next) {
       });
     const recipe = JSON.parse(req.body.recipe);
     const user = await getUserById(id);
+
     if (!user) {
       return res.status(401).json({
         resultMassage: { en: getText('en', '00052') },
@@ -36,30 +37,22 @@ async function addRecipe(req, res, next) {
       });
     }
 
-    // const fileName = req.file.originalname;
-    // const thumb = await Jimp.read(`src/server/tmp/${fileName}`);
-    // const randomName = nanoid();
-    // await thumb.writeAsync(`src/server/public/images/${randomName}${fileName}`);
-    // const preview = thumb.cover(250, 250);
-    // await preview.writeAsync(
-    //   `src/server/public/images/${randomName}${fileName}_preview`
-    // );
-    console.log(req.file);
-    const form = new FormData();
-    const reader = fs.readFile(req.file, 'base64url');
-    form.append('image', reader);
+    const fileName = req.file.originalname;
 
-    const image = await fetch(
-      `https://api.imgbb.com/1/upload/?key=${imageApiKey}`,
-      { method: 'POST', body: form }
+    const image = await imgbbUploader(
+      imageApiKey,
+      `${tmpDir}${fileName}`
+    ).catch(error =>
+      res.status(400).json({
+        resultMassage: 'Something went wrong',
+        resultCode: '00000',
+        error: error.message,
+      })
     );
 
     if (image) {
       newRecipe.thumb = image.url;
       newRecipe.preview = image.display_url;
-    } else {
-      newRecipe.thumb = '';
-      newRecipe.preview = '';
     }
 
     await newRecipe.save();
@@ -72,6 +65,8 @@ async function addRecipe(req, res, next) {
     });
   } catch (error) {
     return next(error);
+  } finally {
+    fs.unlink(`${req.file.path}`);
   }
 }
 
