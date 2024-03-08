33514db15ad39b4f4834c2d28da0993b219c7fbd
mattmalicki <mateusz.malicki96@gmail.com>
2024-03-08 14:55:41 +0100

    Fixing function not working and wrapping it in a try catch.

--- src/api/controllers/user/auth/register.js
+++ src/api/controllers/user/auth/register.js
@@ -15,93 +15,96 @@ const { hash } = bcrypt;
 import geoip from 'geoip-lite';
 const { lookup } = geoip;
 
-export default async (req, res) => {
-  const { error } = validateRegister(req.body);
-  if (error) {
-    let code = '00025';
-    if (error.details[0].message.includes('email')) code = '00026';
-    else if (error.details[0].message.includes('password')) code = '00027';
-    else if (error.details[0].message.includes('name')) code = '00028';
+export default async (req, res, next) => {
+  try {
+    const { error } = validateRegister(req.body);
+    if (error) {
+      let code = '00025';
+      if (error.details[0].message.includes('email')) code = '00026';
+      else if (error.details[0].message.includes('password')) code = '00027';
+      else if (error.details[0].message.includes('name')) code = '00028';
 
-    return res
-      .status(400)
-      .json(errorHelper(code, req, error.details[0].message));
-  }
+      return res
+        .status(400)
+        .json(errorHelper(code, req, error.details[0].message));
+    }
 
-  const exists = await User.exists({ email: req.body.email }).catch(err => {
-    return res.status(500).json(errorHelper('00031', req, err.message));
-  });
-
-  if (exists) return res.status(409).json(errorHelper('00032', req));
-
-  const hashed = await hash(req.body.password, 10);
-
-  // const emailCode = generateRandomCode(4);
-  // await sendCodeToEmail(
-  //   req.body.email,
-  //   req.body.name,
-  //   emailCode,
-  //   (req.body.language = 'en'),
-  //   'register',
-  //   req,
-  //   res
-  // );
-
-  let username = '';
-  let tempName = '';
-  let existsUsername = true;
-  let name = turkishToEnglish(req.body.name);
-  if (name.includes(' ')) {
-    tempName = name.trim().split(' ').slice(0, 1).join('').toLowerCase();
-  } else {
-    tempName = name.toLowerCase().trim();
-  }
-  do {
-    username = tempName + generateRandomCode(4);
-    existsUsername = await User.exists({ username: username }).catch(err => {
-      return res.status(500).json(errorHelper('00033', req, err.message));
+    const exists = await User.exists({ email: req.body.email }).catch(err => {
+      return res.status(500).json(errorHelper('00031', req, err.message));
     });
-  } while (existsUsername);
 
-  const geo = lookup(ipHelper(req));
+    if (exists) return res.status(409).json(errorHelper('00032', req));
 
-  let user = new User({
-    email: req.body.email,
-    password: hashed,
-    name: name,
-    username: username,
-    language: req.body.language,
-    platform: req.body.platform,
-    isVerified: false,
-    countryCode: geo == null ? 'US' : geo.country,
-    timezone: req.body.timezone,
-    lastLogin: Date.now(),
-  });
+    const hashed = await hash(req.body.password, 10);
 
-  user = await user.save().catch(err => {
-    return res.status(500).json(errorHelper('00034', req, err.message));
-  });
+    const emailCode = generateRandomCode(4);
+    // await sendCodeToEmail(
+    //   req.body.email,
+    //   req.body.name,
+    //   emailCode,
+    //   (req.body.language = 'en'),
+    //   'register',
+    //   req,
+    //   res
+    // );
 
-  const confirmCodeToken = signConfirmCodeToken(user._id, emailCode);
-  user.confirmCode = confirmCodeToken;
-  await user.save();
-  await sendCodeToEmail(
-    req.body.email,
-    user.name,
-    confirmCodeToken,
-    user.language,
-    'newCode',
-    req,
-    res
-  );
-  user.password = null;
-  logger('00035', user._id, getText('en', '00035'), 'Info', req);
-  return res.status(200).json({
-    resultMessage: { en: getText('en', '00035'), tr: getText('tr', '00035') },
-    resultCode: '00035',
-    user,
-    confirmToken: confirmCodeToken,
-  });
+    let username = '';
+    let tempName = '';
+    let existsUsername = true;
+    let name = turkishToEnglish(req.body.name);
+    if (name.includes(' ')) {
+      tempName = name.trim().split(' ').slice(0, 1).join('').toLowerCase();
+    } else {
+      tempName = name.toLowerCase().trim();
+    }
+    do {
+      username = tempName + generateRandomCode(4);
+      existsUsername = await User.exists({ username: username }).catch(err => {
+        return res.status(500).json(errorHelper('00033', req, err.message));
+      });
+    } while (existsUsername);
+
+    const geo = lookup(ipHelper(req));
+
+    const user = new User({
+      email: req.body.email,
+      password: hashed,
+      name: name,
+      username: username,
+      language: req.body.language,
+      platform: req.body.platform,
+      isVerified: false,
+      countryCode: geo == null ? 'US' : geo.country,
+      timezone: req.body.timezone,
+      lastLogin: Date.now(),
+    });
+
+    const confirmCodeToken = signConfirmCodeToken(user._id, emailCode);
+    user.confirmCode = confirmCodeToken;
+    await user.save();
+
+    await sendCodeToEmail(
+      req.body.email,
+      user.name,
+      confirmCodeToken,
+      user.language,
+      'newCode',
+      req,
+      res
+    );
+    user.password = null;
+    logger('00035', user._id, getText('en', '00035'), 'Info', req);
+    return res.status(200).json({
+      resultMessage: { en: getText('en', '00035'), tr: getText('tr', '00035') },
+      resultCode: '00035',
+      user,
+      confirmToken: confirmCodeToken,
+    });
+  } catch (error) {
+    return next(error);
+  } finally {
+    console.log('Test');
+  }
 };
 
 /**
