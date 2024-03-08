d68cdce1beea8faa3bc29594d7c9a5cb84e52f3e
mattmalicki <mateusz.malicki96@gmail.com>
2024-03-08 19:34:24 +0100

    Commenting enpoints without auth and uncommenting with auth.

--- src/api/routes/recipe.js
+++ src/api/routes/recipe.js
@@ -7,22 +7,22 @@ import {
   getRecipes,
   getFavorites,
 } from '../controllers/recipe/index.js';
-// import { auth, imageUpload } from '../middlewares/index.js';
+import { auth } from '../middlewares/index.js';
 
 const router = Router();
 
 // CATEGORY
-router.get('/', getRecipes);
-// router.get('/category', auth, getRecipes);
-router.get('/popular', getPopular);
-// router.get('/popular', auth, getPopular);
-router.get('/favorites/', getFavorites);
-// router.get('/favorites', auth, getFavorites);
-router.post('/favorites/:recipeId', addToFavorites);
-// router.post('/favorites', auth, addToFavorites);
-router.delete('/favorites/:recipeId', deleteFromFavorites);
-// router.delete('/favorites/:recipeId', auth, deleteFromFavorites);
-router.get('/:recipeId', getOneRecipe);
-// router.get('/:recipeId', auth, getOneRecipe);
+// router.get('/', getRecipes);
+router.get('/', auth, getRecipes);
+// router.get('/popular', getPopular);
+router.get('/popular', auth, getPopular);
+// router.get('/favorites/', getFavorites);
+router.get('/favorites', auth, getFavorites);
+// router.post('/favorites/:recipeId', addToFavorites);
+router.post('/favorites/:recipeId', auth, addToFavorites);
+// router.delete('/favorites/:recipeId', deleteFromFavorites);
+router.delete('/favorites/:recipeId', auth, deleteFromFavorites);
+// router.get('/:recipeId', getOneRecipe);
+router.get('/:recipeId', auth, getOneRecipe);
 
 export default router;
